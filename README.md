6050dc2b7e80e79ddad426617be12ed6320ac2ab
Andras Klikk <andras.klikk@tipico.com>
2014-05-06 11:54:43 +0200

    Implemented Ajax reload in every 5 seconds

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ supraball-serverlist
 ====================
 
 Web-based server browser for the awesome game SUPRABALL
+
+##Some useful info:
+
+- For local usage you have to enable --allow-file-access-from-files flag in your browser. Google how
--- js/main.js
+++ js/main.js
@@ -36,25 +36,47 @@
 //     }
 // });
 
+// Define variables
+
+var serverList;
+
+// Define functions
+
+function dataReloaded(){
+  console.log("data reloaded");
+}
+
 $(function(){
+
+  // So after DOM loads setup the Ajax request and the datatable
+
   serverList = $('#server-list').DataTable( {
-    data: data.servers,
-    dataSrc: "servers",
-      columns: [
-        { data: 'ip' },
-        { data: 'port' },
-        { data: 'title' },
-        { data: 'description' },
-        { data: 'max_players' },
-        { data: 'max_spectators' },
-        { data: 'max_duration' },
-        { data: 'max_goals' },
-        { data: 'current_players' },
-        { data: 'current_duration' },
-        { data: 'current_goals_red' },
-        { data: 'current_goals_blue' },
-        { data: 'map' },
-        { data: 'training' }
-      ]
+    ajax: {
+      url: "js/example-data.txt",
+      dataSrc: "servers"
+    },
+    columns: [
+      { data: 'ip' },
+      { data: 'port' },
+      { data: 'title' },
+      { data: 'description' },
+      { data: 'max_players' },
+      { data: 'max_spectators' },
+      { data: 'max_duration' },
+      { data: 'max_goals' },
+      { data: 'current_players' },
+      { data: 'current_duration' },
+      { data: 'current_goals_red' },
+      { data: 'current_goals_blue' },
+      { data: 'map' },
+      { data: 'training' }
+    ]
   });
+
+  // Autorefresh in every 5 secs
+
+  window.setInterval(function(){
+    serverList.ajax.reload(dataReloaded);
+  }, 5000);
+
 });
